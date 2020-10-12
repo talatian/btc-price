37fb02034f795d780dc6911dd2769eb16fce4df2
talatian <htalatian@gmail.com>
2020-10-12 14:39:16 +0330

    initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import json
+from time import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+capabilities = DesiredCapabilities.CHROME
+capabilities["goog:loggingPrefs"] = {"performance": "ALL"}  # chromedriver 75+
+
+driver = webdriver.Chrome(
+    r"chromedriver.exe",
+    desired_capabilities=capabilities,
+)
+
+
+def get_btc_prices(logs):
+    """
+    Return only logs which have a method that start with "Network.response", "Network.request", or "Network.webSocket"
+    since we're interested in the network events specifically.
+    """
+    for entry in logs:
+        log = json.loads(entry["message"])["message"]
+        if (
+                "Network.webSocketFrameReceived" in log["method"]
+        ):
+            data = json.loads(log["params"]["response"]["payloadData"])
+            if data.get("stream") == 'btcusdt@aggTrade':
+                btc_price = data.get("data")["p"]
+                yield btc_price
+
+
+driver.get("https://www.binance.com/en/trade/btc_usdt")
+
+element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "showPrice"))
+)
+
+print("Page loaded!")
+
+with open("log.txt", "wt") as out:
+    minutes = 1
+    t_end = time() + 60 * minutes
+    btc_sum = 0
+    counter = 0
+    while time() < t_end:
+        logs = driver.get_log("performance")
+        btc_prices = get_btc_prices(logs)
+        for price in btc_prices:
+            counter += 1
+            print(price, file=out)
+            print(f"#{counter} btc-usd price: {price}")
+            btc_sum += float(price)
+    btc_avg = btc_sum / counter
+    print(f"btc-usd avg. price within {minutes} minutes: {btc_avg}")
+
+driver.close()
