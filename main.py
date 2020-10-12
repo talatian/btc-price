e2e15b38f3cb3992bac78f3d383fb6d121e90c82
talatian <htalatian@gmail.com>
2020-10-12 15:04:25 +0330

    refactor

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 capabilities = DesiredCapabilities.CHROME
 capabilities["goog:loggingPrefs"] = {"performance": "ALL"}  # chromedriver 75+
 
-driver = webdriver.Chrome(
-    r"chromedriver.exe",
-    desired_capabilities=capabilities,
-)
-
 
 def get_btc_prices(logs):
     """
@@ -32,28 +27,33 @@ def get_btc_prices(logs):
                 yield btc_price
 
 
-driver.get("https://www.binance.com/en/trade/btc_usdt")
+def wait_for_price():
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "showPrice"))
+    )
+    print("Page loaded!")
 
-element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "showPrice"))
-)
 
-print("Page loaded!")
+def btc_average(minutes):
+    with open("log.txt", "wt") as out:
+        t_end = time() + 60 * minutes
+        btc_sum = 0
+        counter = 0
+        while time() < t_end:
+            logs = driver.get_log("performance")
+            btc_prices = get_btc_prices(logs)
+            for price in btc_prices:
+                counter += 1
+                print(price, file=out)
+                print(f"#{counter} btc-usd price: {price}")
+                btc_sum += float(price)
+        btc_avg = btc_sum / counter
+        print(f"btc-usd avg. price within {minutes} minutes: {btc_avg}")
 
-with open("log.txt", "wt") as out:
-    minutes = 1
-    t_end = time() + 60 * minutes
-    btc_sum = 0
-    counter = 0
-    while time() < t_end:
-        logs = driver.get_log("performance")
-        btc_prices = get_btc_prices(logs)
-        for price in btc_prices:
-            counter += 1
-            print(price, file=out)
-            print(f"#{counter} btc-usd price: {price}")
-            btc_sum += float(price)
-    btc_avg = btc_sum / counter
-    print(f"btc-usd avg. price within {minutes} minutes: {btc_avg}")
 
-driver.close()
+if __name__ == '__main__':
+    driver = webdriver.Chrome(r"chromedriver.exe", desired_capabilities=capabilities)
+    driver.get("https://www.binance.com/en/trade/btc_usdt")
+    wait_for_price()
+    btc_average(1)
+    driver.close()
